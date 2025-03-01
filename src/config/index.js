8e61b8e463c959e02789cefb1bb5262a531132c3
jossyfresh <jossyfresh2002@gmail.com>
2025-03-01 14:39:54 +0300

    config

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const API_URL = process.env.API_URL;
 export const MINI_APP_URL = process.env.MINI_APP_URL;
+export const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
 if (!MINI_APP_URL) {
   console.error("Error: MINI_APP_URL is missing from environment variables.");
@@ -18,5 +19,10 @@ if (!API_URL) {
 
 if (!BOT_TOKEN) {
   console.error("Error: BOT_TOKEN is missing from environment variables.");
+  process.exit(1);
+}
+
+if (!WEBHOOK_URL){
+  console.error("Error: WEBHOOK_URL is missing from enviroment variables.")
   process.exit(1);
 }
